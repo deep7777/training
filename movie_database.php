1bcf293167be5fe36969bf817c4b78245860f480
Deepak Yadav <deepak@appnexio.com>
2021-08-27 15:42:28 +0530

    movie ticket planning

---
+++ movie_database.php
@@ -0,0 +1,64 @@
+<?php 
+
+/*
+theatre name => Fun Square
+movie name => Big Bull
+picture time => 9 - 12
+movie duration = 3 hours
+interval_time
+language => Hindi
+
+
+ticket charges => 250
+ticket available = 100 seats
+
+
+movies
+----------------
+movie_id
+movie_name
+movie_duration
+interval_time
+movie_date
+movie_time
+movie_language
+
+
+Book_Ticket
+booking_ticket_id
+booking_ticket_number -> 31434344242
+mobile_number -> 3343434343
+username -> Deepak
+seat_id -> 1
+movie_id -> 1
+barcode -> xyz
+booking_ticket_date_time => 20 aug 10am
+
+
+
+
+seat_type
+seat_id
+seat_name premium || gold || silver || Basic
+seat_amount 100 || 200 || 300
+
+
+create table seats
+id
+seat_number => [1..2.100]
+seat_type_id  => 1,3
+
+
+parking
+----------------
+id
+vehicle_number 
+vehicle_type
+slot
+
+
+*/
+
+?>
+
+
